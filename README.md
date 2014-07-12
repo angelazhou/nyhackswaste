5b7204dc105366f4876f47d05928d0665efad725
Angela Zhou <angelaz@princeton.edu>
2014-07-12 12:56:19 -0400

    pages commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Urban composting .. with a twist
---
+++ js/graphs.js
@@ -0,0 +1,25 @@
+$(function () { 
+    $('#container1#graph1').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Compost'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
